51149a1147b1545648aa847fe63e18cb45fa1108
R. S. Doiel <rsdoiel@gmail.com>
2017-10-16 12:44:20 -0700

    Moved saferHttpGet, saferHttpPost functions to saferclient.php

--- example_tests/README.md
+++ example_tests/README.md
@@ -1,6 +1,8 @@
 
 # Example Tests
 
-This directory contains some example test that might be run on a page.
+This directory contains some example test that might be run on a page. This is really pseudo code
+and presented only to show basic ideas about simplified testing for legacy PHP systems.
+
 
 
--- example_tests/check-for-php-notice.php
+++ example_tests/check-for-php-notice.php
@@ -3,7 +3,7 @@
  * check-for-php-notive.php is an example of retrieve a PHP page from http://example.org/login.php
  * and see if any PHP notices show up in the retrieved page.
  */
-require_once("../safer.php");
+require_once("../saferclient.php");
 if (php_sapi_name() != "cli") {
     echo $argv[0] . "Must be run from the command line." . PHP_EOL;
     exit(1);
@@ -12,16 +12,16 @@ if (php_sapi_name() != "cli") {
 // Check the login page without any parameters
 $response = saferHttpGet("https://example.org/login.php");
 if ($response["error"] != "") {
-    dump_var($response);
+    var_dump($response);
     exit(1);
 }
 // Check the response for PHP "Notice" lines
-$lines = split($response["content", "\n"]);
+$lines = split($response["content"], "\n");
 $i = 0;
 foreach ($lines as $line) {
     if (strpos("<b>Notice</b>") !== FALSE) {
        echo "NOTICE: $line"; 
-       $++
+       $i++
     }
 }
 if ($i > 0) {
@@ -35,16 +35,16 @@ $response = saferHttpPost("https://example.org/login.php", array(
  "password" => "some-mighty-secret-thing"
 ));
 if ($response["error"] != "") {
-    dump_var($response);
+    var_dump($response);
     exit(1);
 }
 // Check the response for PHP "Notice" lines
-$lines = split($response["content", "\n"]);
+$lines = split($response["content"], "\n");
 $i = 0;
 foreach ($lines as $line) {
     if (strpos("<b>Notice</b>") !== FALSE) {
        echo "NOTICE: $line"; 
-       $++
+       $i++
     }
 }
 if ($i > 0) {
